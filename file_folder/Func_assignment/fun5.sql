6be73ceeff421096b032b776a256dcde7ead0e96
Omkardivate <112164147+Omkardivate@users.noreply.github.com>
2023-10-21 23:57:29 +0530

    22 oct

--- file_folder/Func_assignment/fun5.sql
+++
@@ -1,9 +0,0 @@
-drop FUNCTION if exists otp_generator;
-delimiter $
-create FUNCTION otp_generator() returns int
-deterministic
-BEGIN
-	SELECT FLOOR(rand()*900000)+100000 INTO @otp;
-	return  @otp;
-end $
-delimiter ;
--- file_folder/Proc_assignment/pla1.sql
+++
@@ -1,8 +0,0 @@
-DROP PROCEDURE IF EXISTS addUser;
-delimiter $
-
-CREATE PROCEDURE addUser(user_nm varchar(20),em VARCHAR(20), pass VARCHAR(15) )
-BEGIN
-	INSERT INTO login VALUES(user_nm,em,pass);
-end $
-delimiter ;
